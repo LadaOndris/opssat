572a1b9d5151972c55d9e104832aa00e9e1db5ab
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-10-08 12:58:16 +0200

    GUI in Tkinter for easy annotation of images

--- src/annotation_tool/core.py
+++ src/annotation_tool/core.py
@@ -1,22 +1,57 @@
+import os
+from pathlib import Path
 from typing import Tuple
 
-
-class Rectangle:
-
-    def __init__(self, x, y, w, h) -> None:
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
+import numpy as np
 
 
-def load_annotations():
-    ...
+class StateStorage:
+
+    def __init__(self, image_path: str, annotations_shape: Tuple[int, int], unannotated_flag=-1):
+        self.annotations_shape = annotations_shape
+        self.unannotated_flag = unannotated_flag
+
+        self.annotation_path = self._image_to_annotation_path(image_path)
+        self.annotations = self.load_annotations()
+
+    def _image_to_annotation_path(self, image_path: str) -> str:
+        path = Path(image_path)
+        file_name_without_extension = path.stem
+        file_path_without_filename = path.parent
+        annotation_path_without_extension = os.path.join(file_path_without_filename, file_name_without_extension)
+        annotation_path = f"{annotation_path_without_extension}.annot.npy"
+        return annotation_path
+
+    def get_annotations(self):
+        return self.annotations
+
+    def load_annotations(self) -> np.ndarray:
+        if os.path.isfile(self.annotation_path):
+            return np.load(self.annotation_path)
+        else:
+            return np.full(self.annotations_shape, fill_value=self.unannotated_flag, dtype=int)
+
+    def save_annotations(self) -> None:
+        np.save(self.annotation_path, self.annotations)
+
+    def print_annotations_stats(self) -> None:
+        annotated_mask = np.where(self.annotations >= 0, 1, 0)
+        annotated_count = np.count_nonzero(annotated_mask)
+        unannotated_count = self.annotations.size - annotated_count
+        print(f"Annotated minitiles:\t{annotated_count}")
+        print(f'Missing annotations:\t{unannotated_count}')
+
+    def set_annotation(self, col: int, row: int, annotation_type: int) -> None:
+        existing_annotation = self.annotations[row, col]
+        if existing_annotation == annotation_type:
+            self.annotations[row, col] = -1
+        else:
+            self.annotations[row, col] = annotation_type
 
 
-def save_annotations(annotations) -> None:
-    ...
+def index_to_pixel(index, minitile_size) -> int:
+    return index * minitile_size
 
 
-def coordinates_to_rectangle(coordinates: Tuple[int, int]) -> Rectangle:
-    ...
+def pixel_to_index(pixel, minitile_size) -> int:
+    return int(pixel // minitile_size)
--- src/annotation_tool/main.py
+++ src/annotation_tool/main.py
@@ -2,12 +2,17 @@ from tkinter import ALL, BOTH, Canvas, Frame, NW, Text, Tk
 
 from PIL import Image, ImageTk
 
+from src.annotation_tool.core import index_to_pixel, pixel_to_index, StateStorage
 
-class Example(Frame):
 
-    def __init__(self, root):
+class UI(Frame):
+
+    def __init__(self, root, img, num_classes, minitile_size: int, state_storage: StateStorage):
         super().__init__()
         self.root = root
+        self.img = img
+        self.num_classes = num_classes
+        self.state_storage = state_storage
 
         self.image_x_start = 0
         self.image_y_start = 0
@@ -15,10 +20,38 @@ class Example(Frame):
         self.right_panel_width = 200
         self.bottom_panel_height = 100
 
-        self.minitile_size = 40
+        self.minitile_size = minitile_size
         self.active_minitile = (0, 0)
+        self.active_type = 0
+
+        self.annotation_tag = 'annotated_minitiles'
+
+        self.drag_x = 0
+        self.drag_y = 0
+
+        self.types = ['Agricultural', 'Cloud', 'Mountain', 'Natural',
+                      'River', 'Sea ice', 'Snow', 'Water']
 
         self.initUI()
+
+    def initUI(self):
+        self.master.title("Image annotation tool")
+        self.pack(fill=BOTH, expand=1)
+        self.photo_image = ImageTk.PhotoImage(self.img)
+
+        canvas = Canvas(self, width=self.img.size[0] + self.right_panel_width,
+                        height=self.img.size[1] + self.bottom_panel_height)
+        self._register_num_key_press(canvas)
+        canvas.bind('<ButtonPress-1>', self._get_on_canvas_mouse_click(canvas))
+        self.draw_image(canvas)
+        self.fill_image_with_minitiles(canvas)
+        self._redraw(canvas)
+        canvas.pack(fill=BOTH, expand=1)
+
+        self.draw_press_space_to_annotate_text(self.root)
+
+    def _register_num_key_press(self, canvas):
+        self.root.bind('<KeyPress>', self.get_on_key_press(canvas))
 
     def get_zoom_function(self, canvas):
         def do_zoom(event):
@@ -30,46 +63,116 @@ class Example(Frame):
 
         return do_zoom
 
-    def get_on_canvas_mouse_click(self, canvas):
+    def _get_on_canvas_mouse_click(self, canvas):
         def on_canvas_mouse_click(event):
-            canvas.scan_mark(event.x, event.y)
-            print(f"clicked at {event.x}, {event.y}")
+            # Adjust click location with respect to canvas' dragged position
+            clicked_x = event.x - index_to_pixel(self.drag_x, self.minitile_size)
+            clicked_y = event.y - index_to_pixel(self.drag_y, self.minitile_size)
 
-            self.update_active_minitile(event.x, event.y)
-            self.highlight_active_minitile(canvas)
+            if self._has_clicked_on_minitile(clicked_x, clicked_y):
+                self._annotate_minitile(clicked_x, clicked_y)
+                self._redraw(canvas)
 
         return on_canvas_mouse_click
 
-    def update_active_minitile(self, x, y):
-        row = self.pixel_to_index(x)
-        col = self.pixel_to_index(y)
-        self.active_minitile = (row, col)
+    def _has_clicked_on_minitile(self, x, y):
+        x_within_bounds = 0 <= x < self.img.size[0]
+        y_within_bounds = 0 <= y < self.img.size[1]
+        return x_within_bounds and y_within_bounds
 
-    def index_to_pixel(self, index) -> int:
-        return index * self.minitile_size
+    def _annotate_minitile(self, x, y):
+        row = pixel_to_index(x, self.minitile_size)
+        col = pixel_to_index(y, self.minitile_size)
+        self.state_storage.set_annotation(col, row, self.active_type)
 
-    def pixel_to_index(self, pixel) -> int:
-        return int(pixel // self.minitile_size)
+    def get_on_key_press(self, canvas):
+        left = 113
+        up = 111
+        right = 114
+        down = 116
 
-    def initUI(self):
-        self.master.title("Image annotation tool")
-        self.pack(fill=BOTH, expand=1)
+        def drag_canvas(key):
+            delta_x = 0
+            delta_y = 0
+            gain = 2
 
-        self.img = Image.open("datasets/opssat/raw/001.png")
-        self.photo_image = ImageTk.PhotoImage(self.img)
+            if key == left:
+                delta_x += gain
+            elif key == right:
+                delta_x -= gain
+            elif key == up:
+                delta_y += gain
+            elif key == down:
+                delta_y -= gain
 
-        canvas = Canvas(self, width=self.img.size[0] + self.right_panel_width,
-                        height=self.img.size[1] + self.bottom_panel_height)
-        canvas.bind("<MouseWheel>", self.get_zoom_function(canvas))
-        canvas.bind('<ButtonPress-1>', self.get_on_canvas_mouse_click(canvas))
-        canvas.bind("<B1-Motion>", lambda event: canvas.scan_dragto(event.x, event.y, gain=1))
+            self.drag_x += delta_x
+            self.drag_y += delta_y
 
-        self.draw_image(canvas)
-        self.fill_image_with_minitiles(canvas)
-        self.highlight_active_minitile(canvas)
-        canvas.pack(fill=BOTH, expand=1)
+            x_pix = index_to_pixel(delta_x, self.minitile_size)
+            y_pix = index_to_pixel(delta_y, self.minitile_size)
+            canvas.scan_mark(0, 0)
+            canvas.scan_dragto(x_pix, y_pix, gain=1)
 
-        self.draw_press_space_to_annotate_text(self.root)
+        def on_key_press(e):
+            if len(e.char) != 0:
+                key = ord(e.char)
+                zero_ascii = 48
+
+                if self._is_existing_type(key - zero_ascii):
+                    self._change_active_type(key - zero_ascii)
+                    self._redraw(canvas)
+
+                if e.char == 's':
+                    print('Saving annotations...')
+                    self.state_storage.save_annotations()
+                    self.state_storage.print_annotations_stats()
+
+            drag_canvas(e.keycode)
+
+
+        return on_key_press
+
+    def _is_existing_type(self, type_index: int) -> bool:
+        return 0 <= type_index < self.num_classes
+
+    def _change_active_type(self, type: int) -> None:
+        self.active_type = type
+        type_name = self.types[type]
+        print(f'Active type {type}: {type_name}')
+
+    def _redraw(self, canvas: Canvas):
+        self._delete_drawn_annotations(canvas)
+        self._draw_annotations(canvas)
+
+    def _delete_drawn_annotations(self, canvas: Canvas) -> None:
+        canvas.delete(self.annotation_tag)
+
+    def _draw_annotations(self, canvas: Canvas) -> None:
+        annotations = self.state_storage.get_annotations()
+        for row in range(annotations.shape[1]):
+            for col in range(annotations.shape[0]):
+                annotation = annotations[col][row]
+                if self._is_annotation(annotation):
+                    color = self._get_annotation_color(annotation)
+                    self._draw_annotation(col, row, canvas, color)
+
+    def _is_annotation(self, annotation: int) -> bool:
+        return 0 <= annotation < self.num_classes
+
+    def _get_annotation_color(self, annotation: int) -> str:
+        if self._is_inactive_annotation(annotation):
+            return "#ab80cf"
+        return "#342469"
+
+    def _is_inactive_annotation(self, annotation: int) -> bool:
+        return annotation != self.active_type
+
+    def _draw_annotation(self, col: int, row: int, canvas: Canvas, color: str) -> None:
+        x = index_to_pixel(col, self.minitile_size)
+        y = index_to_pixel(row, self.minitile_size)
+
+        canvas.create_rectangle(x, y, x + self.minitile_size, y + self.minitile_size,
+                                outline=color, tags=self.annotation_tag, width=3)
 
     def draw_image(self, canvas: Canvas):
         canvas.create_image(self.image_x_start, self.image_y_start, anchor=NW, image=self.photo_image)
@@ -77,12 +180,13 @@ class Example(Frame):
     def fill_image_with_minitiles(self, canvas):
         for row in range(self.image_y_start, self.image_y_start + self.img.size[1], self.minitile_size):
             for col in range(self.image_x_start, self.image_x_start + self.img.size[0], self.minitile_size):
-                canvas.create_rectangle(col, row, self.minitile_size, self.minitile_size, outline="#6e2a2a")
+                canvas.create_rectangle(col, row, self.minitile_size, self.minitile_size, outline="#6b5c5c")
 
     def highlight_active_minitile(self, canvas):
-        col = self.index_to_pixel(self.active_minitile[0])
-        row = self.index_to_pixel(self.active_minitile[1])
-        canvas.create_rectangle(col, row, col + self.minitile_size, row + self.minitile_size, outline="#bf1b1b", width=3)
+        col = index_to_pixel(self.active_minitile[0], self.minitile_size)
+        row = index_to_pixel(self.active_minitile[1], self.minitile_size)
+        canvas.create_rectangle(col, row, col + self.minitile_size, row + self.minitile_size, outline="#bf1b1b",
+                                width=3)
 
     def draw_press_space_to_annotate_text(self, parent):
         y = self.img.size[1] + 50
@@ -94,8 +198,14 @@ class Example(Frame):
 
 def main():
     root = Tk()
-    # root.attributes('-zoomed', True)
-    ex = Example(root)
+
+    minitile_size = 40
+    image_path = "datasets/opssat/raw/001.png"
+    img = Image.open(image_path)
+    minitiles_shape = (pixel_to_index(img.size[0], minitile_size) + 1,
+                       pixel_to_index(img.size[1], minitile_size) + 1)
+    storage = StateStorage(image_path, minitiles_shape)
+    ex = UI(root, img, num_classes=8, minitile_size=minitile_size, state_storage=storage)
     root.mainloop()
 
 
