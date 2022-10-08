7e88ca4c145e9e42a0c92aa66605a43549d31d1c
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-10-08 16:54:21 +0200

    Balanced classes tf.data.Dataset pipeline

--- src/dataset/opssat.py
+++ src/dataset/opssat.py
@@ -27,13 +27,21 @@ class TrainDataset:
     prepares data for training.
     """
 
-    def __init__(self, dataset_path: str):
+    def __init__(self, dataset_path: str, num_classes: int, minitile_size: int, batch_size: int):
         self.dataset_path = dataset_path
+        self.num_classes = num_classes
+        self.minitile_size = minitile_size
+        self.batch_size = batch_size
 
         self.annotations = self._load_annotations()
         self._fill_unannotated_minitiles()
         self._check_annotations()
 
+        self.image_file_paths = tf.convert_to_tensor([key for key in self.annotations.keys()])
+        self.image_annotations = tf.convert_to_tensor([value for value in self.annotations.values()])
+        self._check_all_classes_are_present()
+
+        self.iterator = self._build_iterator()
 
     def _load_annotations(self):
         """
@@ -79,18 +87,85 @@ class TrainDataset:
                 raise RuntimeError(f'Invalid annotation file for image: {image_path}. '
                                    f'Missing annotations: {unannotated_count}')
 
-    def _make_dataset(self):
+    def _check_all_classes_are_present(self):
+        for class_idx in range(self.num_classes):
+            class_exists = tf.reduce_any(self.image_annotations == class_idx)
+            if not class_exists:
+                raise RuntimeError(f"Class {class_idx} is not present in training data.")
+
+    def _build_iterator(self):
         """
         Prepares tf.data.Dataset.
         """
-        # Keep all images with .annot.npy files in memory
+        datasets_for_each_class = []
 
-        # Randomly select tiles from images and their corresponding annotations
-        # until a full batch
+        for class_idx in range(self.num_classes):
+            dataset = tf.data.Dataset.from_tensors(self.image_file_paths)
+            dataset = dataset.repeat()
+            dataset = dataset.map(self._prepare_sample)
+            dataset = dataset.filter(self._get_filter_class_predicate(class_idx))
+            datasets_for_each_class.append(dataset)
+
+        # Combine datasets together to get evenly represented classes
+        choice_dataset = tf.data.Dataset.range(self.num_classes).repeat()
+        combined_dataset = tf.data.Dataset.choose_from_datasets(datasets_for_each_class, choice_dataset)
+        combined_dataset = combined_dataset.batch(self.batch_size)
+        combined_dataset = combined_dataset.shuffle(self.batch_size)
 
         # Perform image augmentation
-        ...
+
+        return combined_dataset
+
+    def _prepare_sample(self, img_file_paths):
+        # Select random image
+        num_files = tf.shape(img_file_paths)[0]
+        random_number = tf.random.uniform(shape=[1], maxval=num_files, dtype=tf.int32)[0]
+        image_annotation = self.image_annotations[random_number]
+
+        # Select random tile 200x200 pixels
+        tile_size = 200
+        num_minitiles = int(tile_size // self.minitile_size)
+        num_cols = tf.shape(image_annotation)[0]
+        num_rows = tf.shape(image_annotation)[1]
+        random_col = tf.random.uniform(shape=[1], maxval=num_cols - num_minitiles, dtype=tf.int32)[0]
+        random_row = tf.random.uniform(shape=[1], maxval=num_rows - num_minitiles, dtype=tf.int32)[0]
+
+        subimage_annotations = image_annotation[random_row:random_row + num_minitiles,
+                               random_col:random_col + num_minitiles]
+
+        # Create label of the majority of votes
+        label = self._annotations_to_label(subimage_annotations)
+
+        # Read image
+        image_raw = tf.io.read_file(img_file_paths[random_number])
+        image = tf.io.decode_png(image_raw, channels=3)
+
+        # Extract tile
+        tile_pixel_row = random_row * self.minitile_size
+        tile_pixel_col = random_col * self.minitile_size
+        tile = image[tile_pixel_row:tile_pixel_row + tile_size, tile_pixel_col:tile_pixel_col + tile_size, :]
+
+        return tile, label
+
+    def _get_filter_class_predicate(self, class_idx: int):
+
+        def filter_class_predicate(tile, label) -> bool:
+            return label == class_idx
+
+        return filter_class_predicate
+
+    def _annotations_to_label(self, annotations):
+        y, idx, count = tf.unique_with_counts(tf.reshape(annotations, [-1]))
+        river_label = 4
+        if tf.reduce_any(y == river_label):
+            return river_label
+        most_votes_arg = tf.argmax(count)
+        return y[most_votes_arg]
 
 
 if __name__ == "__main__":
-    dataset = TrainDataset('datasets/opssat/raw')
+    dataset = TrainDataset('datasets/opssat/raw', num_classes=8, minitile_size=40, batch_size=32)
+    for tile, annot in dataset.iterator:
+        print(tile)
+        print(annot)
+        pass
