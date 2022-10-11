f619184354f6c3b63b2ff80cfe44a3b899007f10
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-10-11 23:10:57 +0200

    Fixed class weight creation #2

--- src/dataset/balancing.py
+++ src/dataset/balancing.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 def compute_class_weight(counts: Dict[str, int]) -> Dict[str, int]:
-    labels = list(counts.keys())
     counts_np = np.array(list(counts.values()))
     counts_sum = np.sum(counts_np)
     num_classes = np.shape(counts_np)[0]
     class_weight = counts_sum / (num_classes * counts_np)
-    class_weight_dict = {labels[i]: class_weight[i] for i in range(class_weight.shape[0])}
+    class_weight_dict = {i: class_weight[i] for i in range(class_weight.shape[0])}
     return class_weight_dict
