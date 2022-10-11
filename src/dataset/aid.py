fe992aef6162c9ebbdad344795db25e45dca8960
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-10-11 23:09:53 +0200

    Fixed class weight creation

--- src/dataset/aid.py
+++ src/dataset/aid.py
@@ -3,9 +3,9 @@ AID: A scene classification dataset
 """
 import glob
 import os
-from typing import Tuple
+from pathlib import Path
+from typing import Dict, Tuple
 
-import numpy as np
 import tensorflow as tf
 
 from src.dataset.balancing import compute_class_weight
@@ -29,13 +29,15 @@ class AID:
             image_size=self.image_size, validation_split=self.validation_split, subset="both")
         return dataset
 
-    def _compute_class_weights(self) -> np.ndarray:
-        image_counts = []
+    def _compute_class_weights(self) -> Dict:
+        image_counts = {}
         subdirs = glob.glob(os.path.join(self.dataset_path, "*"))
         for subdir in subdirs:
+            label = Path(subdir).stem
             files = glob.glob(os.path.join(subdir, "*"))
-            image_counts.append(len(files))
-        return compute_class_weight(image_counts)
+            image_counts[label] = len(files)
+        class_weight = compute_class_weight(image_counts)
+        return class_weight
 
 
 if __name__ == "__main__":
--- src/dataset/balancing.py
+++ src/dataset/balancing.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import Dict
 
 import numpy as np
 
 
-def compute_class_weight(counts: List[int]):
-    counts_np = np.array(counts)
+def compute_class_weight(counts: Dict[str, int]) -> Dict[str, int]:
+    labels = list(counts.keys())
+    counts_np = np.array(list(counts.values()))
     counts_sum = np.sum(counts_np)
     num_classes = np.shape(counts_np)[0]
     class_weight = counts_sum / (num_classes * counts_np)
-    return class_weight
+    class_weight_dict = {labels[i]: class_weight[i] for i in range(class_weight.shape[0])}
+    return class_weight_dict
