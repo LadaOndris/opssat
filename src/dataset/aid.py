5fdb99bb1b1fe809a80d1bcc8fc76a2409ddaa2a
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-10-15 20:47:34 +0200

    Augmentation pipeline

--- src/dataset/aid.py
+++ src/dataset/aid.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from typing import Dict, Tuple
 
 import tensorflow as tf
+import tensorflow.python.keras.layers as layers
+from tensorflow.keras import Sequential
 
 from src.dataset.balancing import compute_class_weight
 
@@ -21,19 +23,39 @@ class AID:
         self.validation_split = validation_split
 
         self.class_weights = self._compute_class_weights()
-        self.train_iterator, train_samples = self._build_iterator('training')
-        self.validation_iterator, val_samples = self._build_iterator('validation')
+        self.train_iterator, train_samples = self._build_iterator('training', augment=True)
+        self.validation_iterator, val_samples = self._build_iterator('validation', augment=False)
 
         self.train_steps = self._get_steps_per_epoch(train_samples)
         self.validation_steps = self._get_steps_per_epoch(val_samples)
 
-    def _build_iterator(self, dataset_subset: str):
+    def _build_iterator(self, dataset_subset: str, augment: bool):
+        augmentation_pipe = self._get_augmentation_pipeline()
+
         dataset = tf.keras.utils.image_dataset_from_directory(
             self.dataset_path, batch_size=self.batch_size, seed=42,
             image_size=self.image_size, validation_split=self.validation_split, subset=dataset_subset)
         dataset_size = dataset.cardinality().numpy()
         dataset = dataset.repeat()
+        if augment:
+            dataset = dataset.map(lambda x, y: (augmentation_pipe(x), y),
+                                  num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
         return dataset, dataset_size
+
+    def _get_augmentation_pipeline(self):
+        augmentation_pipeline = Sequential([
+            layers.RandomContrast(0.2),
+            layers.RandomFlip("horizontal_and_vertical"),
+            layers.RandomZoom(
+                height_factor=(-0.2, 0.2),
+                width_factor=(-0.2, 0.2)),
+            layers.RandomRotation(1.0),  # rotate randomly in the 360 degree range
+            layers.RandomTranslation(
+                height_factor=(-0.2, 0.2),
+                width_factor=(-0.2, 0.2)),
+        ])
+        return augmentation_pipeline
 
     def _compute_class_weights(self) -> Dict:
         image_counts = {}
@@ -54,5 +76,12 @@ class AID:
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import matplotlib
+    matplotlib.use('TkAgg')
+
     aid = AID('datasets/AID/', batch_size=8, image_size=(200, 200))
-    pass
+    for im, label in aid.train_iterator:
+        plt.imshow(im[0].numpy().astype(int))
+        plt.show()
+        pass
